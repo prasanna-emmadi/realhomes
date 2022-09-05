038fc8b76c28e239def4989878ffbd8edf8a1abf
Prasanna_emmadi <prasanna.177@gmail.com>
2022-09-05 19:51:09 +0300

    Removing Grid based Home

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -9,7 +9,7 @@ import PropertyListing from "./PropertyListing";
 import SearchBar from "./components/SearchBar";
 import { DataContext } from "../context/DataContext.js";
 
-const Home1 = () => {
+const Home = () => {
   const { searchOptions } = useContext(DataContext);
   return (
     <Box
@@ -62,83 +62,4 @@ const Home1 = () => {
   );
 };
 
-/*const Home = () => {
-  const { searchOptions } = useContext(DataContext);
-
-  return (
-    <Grid
-      templateAreas={`
-                    "main main"
-                    "search search"
-                    "buy sell"
-                    "list list"`}
-      gridTemplateRows={" 600px 100px 400px 1fr"}
-      gridTemplateColumns={"500px 500px"}
-      gap="1"
-      color="blackAlpha.700"
-      fontWeight="bold"
-    >
-      <GridItem pl="2" bg="white.300" area={"main"}>
-        <Box boxSize="1000px">
-          <Image src={MainImage} alt="Real Homes" />
-        </Box>
-      </GridItem>
-      <GridItem pl="2" bg="grey.300" area={"search"}>
-        <Box
-          zIndex={10}
-          position="relative"
-          marginTop={"-12"}
-          marginBottom={"-16"}
-        >
-          <SearchBar />
-        </Box>
-      </GridItem>
-      <GridItem pl="2" bg="white.300" area={"buy"}>
-        <div className={styles.headtext}>
-          <Box
-            boxSize=""
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            flexDirection="column"
-          >
-            <Link to="/buyhome">
-              <Image
-                boxSize="300px"
-                objectFit="cover"
-                src={BuyImage}
-                alt="Buy"
-              />
-            </Link>
-            <Text fontSize="xl">Buy a Home</Text>
-          </Box>
-        </div>
-      </GridItem>
-      <GridItem pl="2" bg="white.300" area={"sell"}>
-        <div className={styles.headtext}>
-          <Box
-            boxSize=""
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            flexDirection="column"
-          >
-            <Link to="/sellhome">
-              <Image
-                boxSize="300px"
-                objectFit="cover"
-                src={SellImage}
-                alt="Sell"
-              />
-            </Link>
-            <Text fontSize="xl">Sell a Home</Text>
-          </Box>
-        </div>
-      </GridItem>
-      <GridItem pl="2" bg="white.300" area={"list"}>
-        <PropertyListing columns={3} searchOptions={searchOptions} />
-      </GridItem>
-    </Grid>
-  );
-};*/
-export default Home1;
+export default Home;
